c029c56db072dc9d6d8cb966b894c6ac03e416d3
y4shiro <y4shironao@gmail.com>
2021-12-21 15:56:56 +0900

    useState と localStorage を使った処理をカスタムフックに切り出した

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { useStateWithStorage } from '../hooks/use_state_with_storage';
 
-const { useState } = React;
 const StorageKey = 'pages/editor:text';
 
 const Header = styled.header`
@@ -47,20 +47,14 @@ const Preview = styled.div`
 `;
 
 export const Editor: React.FC = () => {
-  const [text, setText] = useState<string>(
-    localStorage.getItem(StorageKey) || ''
-  );
+  const [text, setText] = useStateWithStorage('', StorageKey);
 
   return (
     <>
       <Header>Markdown Editor</Header>
       <Wrapper>
         <TextArea
-          onChange={(event) => {
-            const changedText = event.target.value;
-            localStorage.setItem(StorageKey, changedText);
-            setText(changedText);
-          }}
+          onChange={(event) => setText(event.target.value)}
           value={text}
         />
         <Preview>プレビューエリア</Preview>
