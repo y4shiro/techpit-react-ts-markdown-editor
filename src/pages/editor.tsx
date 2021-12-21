598b943c51c5655a503ee7757e9bff0954a0f6b4
y4shiro <y4shironao@gmail.com>
2021-12-21 16:03:15 +0900

    react-markdown をプレビュー画面に組み込み

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import { useStateWithStorage } from '../hooks/use_state_with_storage';
+import * as ReactMarkdown from 'react-markdown';
 
 const StorageKey = 'pages/editor:text';
 
@@ -57,7 +58,9 @@ export const Editor: React.FC = () => {
           onChange={(event) => setText(event.target.value)}
           value={text}
         />
-        <Preview>プレビューエリア</Preview>
+        <Preview>
+          <ReactMarkdown>{text}</ReactMarkdown>
+        </Preview>
       </Wrapper>
     </>
   );
