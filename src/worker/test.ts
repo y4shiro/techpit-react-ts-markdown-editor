d91f2a80405f2417d0f16b5047143e60269f9406
y4shiro <y4shironao@gmail.com>
2021-12-22 16:14:09 +0900

    重たい処理をメインスレッドから Web worker に移す

--- src/worker/test.ts
+++ src/worker/test.ts
@@ -2,5 +2,11 @@ const worker: Worker = self as any;
 
 worker.addEventListener('message', (event) => {
   console.log('Worker Received:', event.data);
+
+  // let count: number = 1;
+  // while (count < 1_000_000_000) {
+  //   count++;
+  // }
+
   worker.postMessage({ result: event.data });
 });
